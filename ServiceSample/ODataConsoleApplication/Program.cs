5c304cd8de8a9ea2e5c40aae0c2e796a6a9943ec
Petr Janík <Petr.Janik@autocont.cz>
2022-03-02 18:00:47 +0100

    Service sample template

---
+++ ServiceSample/ODataConsoleApplication/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using AuthenticationUtility;
+using Microsoft.OData.Client;
+using ODataUtility.Microsoft.Dynamics.DataEntities;
+
+namespace ODataConsoleApplication
+{
+    class Program
+    {
+        public static string ODataEntityPath = ClientConfiguration.Default.UriString + "data";
+
+        static void Main(string[] args)
+        {
+            // To test custom entities, regenerate "ODataClient.tt" file.
+            // https://blogs.msdn.microsoft.com/odatateam/2014/03/11/tutorial-sample-how-to-use-odata-client-code-generator-to-generate-client-side-proxy-class/
+
+            //TODO: Force TLS 1.2
+
+            Uri oDataUri = new Uri(ODataEntityPath, UriKind.Absolute);
+            var context = new Resources(oDataUri);
+
+            context.SendingRequest2 += new EventHandler<SendingRequest2EventArgs>(delegate (object sender, SendingRequest2EventArgs e)
+            {
+                var authenticationHeader = OAuthHelper.GetAuthenticationHeader(useWebAppAuthentication: true);
+                e.RequestMessage.SetHeader(OAuthHelper.OAuthHeader, authenticationHeader);
+            });
+
+
+           //TODO: Read OData enity and do some action on it
+
+            Console.ReadLine();
+        }
+    }
+}
